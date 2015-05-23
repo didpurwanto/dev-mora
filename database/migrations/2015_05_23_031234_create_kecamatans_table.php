283e45fae1d2411d26c04adaa5a7f0375f02b12a
mhixv <mhi.henri@yahoo.com>
2015-05-23 10:19:51 +0700

    Add Provinsi Kabupaten Kecamatan

---
+++ database/migrations/2015_05_23_031234_create_kecamatans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKecamatansTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('kecamatans', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('kabupaten_id');
+			$table->string('kecamatan_name');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('kecamatans');
+	}
+
+}
