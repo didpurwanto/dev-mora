559799574e759f67e44952fa9e8ba9b05604c298
indi60 <agung.smarts@gmail.com>
2015-06-03 21:39:02 +0700

    update schema table families

--- database/migrations/2015_05_11_155116_create_families.php
+++ database/migrations/2015_05_11_155116_create_families.php
@@ -15,20 +15,27 @@ class CreateFamilies extends Migration {
 		Schema::create('families', function($table)
 		{
 			$table->increments('id');
+			$table->integer('applicant_id');
+
 			$table->string('father_name', 50);
 			$table->string('father_age', 100);
 			$table->boolean('father_deceased');
-			$table->string('father_address', 100);
-			$table->integer('father_province_id');
+			$table->integer('father_education');
 			$table->integer('father_job_id');
-			$table->integer('father_income_id');
+			$table->integer('father_salary_id');
+
 			$table->string('mother_name', 50);
 			$table->string('mother_age', 100);
 			$table->boolean('mother_deceased');
-			$table->string('mother_address', 100);
-			$table->integer('mother_province_id');
+			$table->integer('mother_education');
 			$table->integer('mother_job_id');
 			$table->integer('mother_income_id');
+
+			$table->string('address', 100);
+			$table->integer('province_id');
+			$table->integer('kabupaten_id');
+			$table->integer('kecamatan_id');
+
 			$table->timestamps();	
 		});
 	}
