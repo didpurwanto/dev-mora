2df59e2de7069bc415786d287214ff302c1cdca1
indi60 <agung.smarts@gmail.com>
2015-06-03 22:10:02 +0700

    create raports table

---
+++ database/migrations/2015_06_03_220347_create_raports.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRaports extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('raports', function($table)
+		{
+			$table->increments('id');
+			$table->integer('applicant_id');
+			$table->string('subject_1', 50);
+			$table->string('subject_2', 50);
+			$table->string('subject_3', 50);
+			$table->string('subject_4', 50);
+			$table->string('subject_5', 50);
+			$table->string('ranking', 50);
+			$table->timestamps();			
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('raports');
+	}
+
+}
