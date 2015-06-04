72fe99fbcd6b03c8390078d6da044b31cbe7684a
mhixv <mhi.henri@yahoo.com>
2015-06-04 09:55:31 +0700

    Create Model JobType EducationLevel Range Salary

---
+++ app/EducationLevel.php
@@ -0,0 +1,9 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EducationLevel extends Model {
+
+	protected $table = 'education_levels';
+
+}
---
+++ app/RangeSalary.php
@@ -0,0 +1,9 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RangeSalary extends Model {
+
+	protected $table = 'range_salaries';
+
+}
