396ed06bdee7dbe9371e9d78b6d1d470f85921fe
mhixv <mhi.henri@yahoo.com>
2015-11-13 07:25:29 +0700

    Add field to check if data finish

---
+++ database/migrations/2015_11_13_072227_add_finish_to_raports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFinishToRaportsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('raports', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('raports', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
