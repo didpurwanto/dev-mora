0e87b2f0c8bcc34d89a57ee27b7d11fab88f14cc
mhixv <mhi.henri@yahoo.com>
2015-12-14 21:42:39 +0700

    try to fix bug : 41 and 42

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -16,17 +16,17 @@ class Registrar implements RegistrarContract {
 	{
 		return Validator::make($data, [
 			'username' => 'required|max:20|min:3|unique_with:users,email',
-			'full_name' => 'required|max:100|min:5|unique:applicants',
-			'email' => 'required|email|max:100',
+			'full_name' => 'required|max:100|min:5|unique',
+			'email' => 'required|unique|email|max:100',
 			'password' => 'required|confirmed|min:8',
 		], [
 			'username.required' => 'Username tidak boleh kosong.',
 			'unique_with' => 'Data-data yang anda masukan sudah terdaftar. Silahkan klik lupa password pada saat login, jika anda sudah pernah mendaftar.',
-			//'email.unique' => 'Username ini sudah digunakan.',
+			'email.unique' => 'Username ini sudah digunakan.',
 			'username.min' => 'Username minimal 3 karakter.',
 			'username.max' => 'Username maksimal 20 karakter.',
 			'full_name.required' => 'Nama Lengkap tidak boleh kosong',
-			//'full_name.unique' => 'Nama Lengkap ini sudah digunakan.',
+			'full_name.unique' => 'Nama Lengkap ini sudah digunakan.',
 			'full_name.min' => 'Nama Lengkap minimal 5 karakter.',
 			'full_name.max' => 'Nama Lengkap maksimal 100 karakter.',
 			'email.required' => 'Alamat email tidak boleh kosong.',
